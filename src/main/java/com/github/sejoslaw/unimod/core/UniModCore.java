8e8a4d2a561d31847b08471484d5055121a24eed
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2019-02-10 07:20:30 +0100

    Added api jar building;

--- src/main/java/com/github/sejoslaw/unimod/core/UniModCore.java
+++ src/main/java/com/github/sejoslaw/unimod/core/UniModCore.java
@@ -7,7 +7,7 @@ package com.github.sejoslaw.unimod.core;
  */
 public final class UniModCore {
 	public static final String MOD_ID = "unimod";
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "0.0.1";
 	public static final String NAME = "UniMod";
 
 	private UniModCore() {
